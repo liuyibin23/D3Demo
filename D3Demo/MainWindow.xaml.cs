c76f0f2f2b4dd5abca8a18085a305c7ccfea8c58
liuyibin <liuyibin23@163.com>
2018-03-15 00:48:28 +0800

    绘制框架的点

---
+++ D3Demo/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using InteractiveDataDisplay.WPF;
+
+namespace D3Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //0
+            DrawArea(Brushes.Red, new Point(14190.491, 6742.325), new Point(14183.954, 6743.655), new Point(14184.054, 6744.145), new Point(14190.591, 6742.815));
+            //1
+            DrawArea(Brushes.Blue, new Point(14184.054, 6744.145), new Point(14186.744, 6757.991), new Point(14193.375, 6756.815), new Point(14190.591, 6742.815));
+            //2
+            DrawArea(Brushes.Red, new Point(14186.744, 6757.991), new Point(14186.866, 6758.680), new Point(14193.497, 6757.504), new Point(14193.375, 6756.815));
+            //3
+            DrawArea(Brushes.Blue, new Point(14191.617, 6747.879), new Point(14192.077, 6750.163), new Point(14197.146, 6749.081), new Point(14196.705, 6746.835));
+
+
+
+            Chart1.PlotOriginX = 14180;
+            Chart1.PlotOriginY = 6740;
+            double height = 20;
+            Chart1.PlotHeight = height;
+            Chart1.PlotWidth = this.Width / this.Height * height;
+            Chart1.SizeChanged += Chart1_SizeChanged;
+
+            //Plot1.Children.OfType<MouseNavigation>().ToList()[0]
+            //mouse.MouseMove += Mouse_MouseMove;
+        }
+
+        private void Chart1_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Chart1.PlotWidth = e.NewSize.Width / e.NewSize.Height * Chart1.PlotHeight;
+        }
+
+        private void DrawArea(Brush color,params Point[] points)
+        {
+            Polygon polygon = new Polygon();
+            PointCollection pc = new PointCollection();
+            List<MapPoint> mps = new List<MapPoint>();
+            double pointR = 15;
+            
+            foreach (var p in points)
+            {
+                pc.Add(p);
+                //mps.Add(new MapPoint(p.X, p.Y, pointR));
+                //MapPoint mp = new MapPoint(p.X,p.Y, pointR);
+                //Plot1.Children.Add(mp.Shape);
+            }
+
+            polygon.Stroke = color;
+            polygon.StrokeThickness = 2;
+
+            
+
+            Plot.SetPoints(polygon, pc);
+            Plot1.Children.Add(polygon);
+            DrawMapPoints(points);
+        }
+
+        private void DrawMapPoints(IEnumerable<Point> points)
+        {
+            double pointR = 15;
+            foreach (var p in points)
+            {
+                MapPoint mp = new MapPoint(p.X, p.Y, pointR);
+                Plot1.Children.Add(mp.Shape);
+            }
+        }
+
+        private void Chart1_Loaded(object sender, RoutedEventArgs e)
+        {
+            //double height = 20;
+            //Chart1.PlotHeight = height;
+            //Chart1.PlotWidth = Chart1.Width / Chart1.Height * height;
+            //Chart1.SizeChanged += Chart1_SizeChanged;
+        }
+
+        private void Plot1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var p = e.GetPosition(sender as IInputElement);
+            var x = Plot1.XFromLeft(p.X);
+            var y = Plot1.YFromTop(p.Y);
+            coordinateTB.Text = $"{x},{y}";
+            Console.WriteLine($"{x},{y}");
+        }
+
+        //private void MouseNavigation_MouseMove(object sender, MouseEventArgs e)
+        //{
+
+        //}
+    }
+}
---
+++ D3Demo/MapPoint.cs
@@ -0,0 +1,89 @@
+﻿using InteractiveDataDisplay.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace D3Demo
+{
+    public class MapPoint
+    {
+        private Brush _color = Brushes.BlueViolet;
+        private double _r = 1;
+        /// <summary>
+        /// 地图点形状
+        /// </summary>
+        public Ellipse Shape { get; private set; } = new Ellipse();
+        //半径
+        public double R
+        {
+            get
+            {
+                return _r;
+            }
+            set
+            {
+                _r = value;
+                Shape.Height = _r;
+                Shape.Width = _r;
+                //根据新半径重新设置一次坐标
+                SetXY(X,Y);
+            }
+        }
+
+        public double X { get; private set;}
+
+        public double Y { get; private set; }
+
+        public Brush Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                Shape.Fill = _color;
+                Shape.Stroke = _color;
+            }
+        }
+
+        public MapPoint(double x, double y, double r)
+        {
+            _r = r;
+            Shape.Height = _r;
+            Shape.Width = _r;
+            SetXY(x,y);
+            Shape.Fill = _color;
+            Shape.Stroke = _color;
+            Shape.MouseEnter += Shape_MouseEnter;
+        }
+
+        private void Shape_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var ellipse = sender as Ellipse;
+            var toolTip = new ToolTip();
+            TextBlock tb = new TextBlock();
+            //var p = (Point)ellipse.Tag;
+            tb.Text = $"{X},{Y}";
+            toolTip.Content = tb;
+            ellipse.ToolTip = toolTip;
+        }
+
+        public void SetXY(double x,double y)
+        {
+            X = x;
+            Y = y;
+            Plot.SetX1(Shape, X);
+            Plot.SetY1(Shape, Y);
+            //X,Y设置的是圆点的Left和Top点，平移后，将X,Y表示为圆点的圆心点。
+            TranslateTransform t = new TranslateTransform();
+            t.X = -_r / 2;
+            t.Y = -_r / 2;
+            Shape.RenderTransform = t;
+        }
+
+    }
+}
